d34a9e45ad5c91a31afa7a84f68707ec4323f9b0
Born1uus <bornaamital@gmail.com>
2021-11-12 22:25:24 +0100

    Initial commit

---
+++ src/main/java/com/borniuus/tensura/item/ModItems.java
@@ -0,0 +1,22 @@
+package com.borniuus.tensura.item;
+
+import com.borniuus.Tensura;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fmllegacy.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Tensura.MOD_ID);
+
+    public static final RegistryObject<Item> SILVER_INGOT = ITEMS.register("silver_ingot",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+
+    }
+}
