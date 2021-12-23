34424ada9a5bf68b5658a25d3a08fd50c904a620
Christian Raoulis <craoulis@googlemail.com>
2021-12-24 00:02:00 +0100

    rename ModCreativeModeTab to TensuraCreativeTab to match forge conventions

--- src/main/java/com/borniuus/tensura/item/TensuraCreativeTab.java
+++ src/main/java/com/borniuus/tensura/item/TensuraCreativeTab.java
@@ -1,40 +1,37 @@
 package com.borniuus.tensura.item;
 
-import com.borniuus.tensura.block.ModBlocks;
+import com.borniuus.tensura.block.TensuraBlocks;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
-public class ModCreativeModeTab {
+public class TensuraCreativeTab {
     public static final CreativeModeTab TENSURA_TAB = new CreativeModeTab("tensuraTab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModItems.MAGISTEEL_INGOT.get());
+            return new ItemStack(TensuraItems.MAGISTEEL_INGOT);
         }
-
     };
 
 
     public static final CreativeModeTab TENSURA_TAB2 = new CreativeModeTab("tensuraTab2") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModBlocks.MAGISTEEL_BLOCK.get());
+            return new ItemStack(TensuraBlocks.MAGISTEEL_BLOCK);
         }
-
     };
 
 
     public static final CreativeModeTab TENSURA_TAB3 = new CreativeModeTab("tensuraTab3") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModItems.RAW_ANT_LEG.get());
+            return new ItemStack(TensuraItems.RAW_ANT_LEG);
         }
-
     };
 
     public static final CreativeModeTab TENSURA_TAB4 = new CreativeModeTab("tensuraTab4") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModItems.MAGISTEEL_PICKAXE.get());
+            return new ItemStack(TensuraItems.MAGISTEEL_PICKAXE);
         }
     };
 
@@ -42,17 +39,15 @@ public class ModCreativeModeTab {
     public static final CreativeModeTab TENSURA_TAB5 = new CreativeModeTab("tensuraTab5") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModItems.MAGISTEEL_SWORD.get());
+            return new ItemStack(TensuraItems.MAGISTEEL_SWORD);
         }
-
     };
 
     public static final CreativeModeTab TENSURA_TAB6 = new CreativeModeTab("tensuraTab6") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModItems.RAW_ANT_LEG.get());
+            return new ItemStack(TensuraItems.RAW_ANT_LEG);
         }
-
     };
 }
 
