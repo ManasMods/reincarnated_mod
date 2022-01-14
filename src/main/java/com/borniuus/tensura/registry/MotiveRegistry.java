23feb35cd03f92dda22f57c42942cfd528d865a6
Christian Raoulis <craoulis@googlemail.com>
2022-01-14 22:20:08 +0100

    add Painting Registry to TensuraRegistry

---
+++ src/main/java/com/borniuus/tensura/registry/MotiveRegistry.java
@@ -0,0 +1,16 @@
+package com.borniuus.tensura.registry;
+
+import com.borniuus.tensura.motive.TensuraMotive;
+import net.minecraft.world.entity.decoration.Motive;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+
+class MotiveRegistry {
+    /**
+     * This Method will register all Motives to Forge.
+     * It is called though the {@link TensuraRegistry#register(IEventBus)} Method.
+     */
+    static void register(DeferredRegister<Motive> registry) {
+        registry.register("example", () -> new TensuraMotive(80, 64));
+    }
+}
--- src/main/java/com/borniuus/tensura/registry/TensuraRegistry.java
+++ src/main/java/com/borniuus/tensura/registry/TensuraRegistry.java
@@ -2,6 +2,7 @@ package com.borniuus.tensura.registry;
 
 import com.borniuus.tensura.Tensura;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.entity.decoration.Motive;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -15,6 +16,7 @@ public class TensuraRegistry {
     private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Tensura.MOD_ID);
     private static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, Tensura.MOD_ID);
     private static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, Tensura.MOD_ID);
+    private static final DeferredRegister<Motive> MOTIVE = DeferredRegister.create(ForgeRegistries.PAINTING_TYPES, Tensura.MOD_ID);
 
     /**
      * Registers all registries.
@@ -27,11 +29,13 @@ public class TensuraRegistry {
         ItemRegistry.register(ITEMS); //Register Items to our Registry
         BlockEntityTypeRegistry.register(ITEMS, BLOCKS, BLOCK_ENTITY_TYPES);
         SoundEventRegistry.register(SOUND_EVENTS); //Register Sound Events
+        MotiveRegistry.register(MOTIVE); //Register Motives for custom paintings
 
         // Add our Registries to Forge
         BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
         BLOCK_ENTITY_TYPES.register(modEventBus);
         SOUND_EVENTS.register(modEventBus);
+        MOTIVE.register(modEventBus);
     }
 }
