8a883406e6e1b876a7d579d978f7a33b5aa2544f
Born1uus <bornasmital@outlook.com>
2022-01-14 20:55:30 +0100

    update #6

--- src/main/java/com/borniuus/tensura/item/custom/IceBladeItem.java
+++ src/main/java/com/borniuus/tensura/item/custom/IceBladeItem.java
@@ -2,16 +2,34 @@ package com.borniuus.tensura.item.custom;
 
 
 import com.borniuus.tensura.item.templates.SimpleSwordItem;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 
 public class IceBladeItem extends SimpleSwordItem {
     public IceBladeItem(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier) {
         super(pTier, pAttackDamageModifier, pAttackSpeedModifier);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents,
+                                TooltipFlag pIsAdvanced) {
+        if (Screen.hasShiftDown()) {
+            pTooltipComponents.add(new TranslatableComponent("tooltip.tensura.iceblade.tooltip.shift"));
+        } else {
+            pTooltipComponents.add(new TranslatableComponent("tooltip.tensura.iceblade.tooltip"));
+        }
     }
 
     @Override
