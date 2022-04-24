b84b79bd4448a0aa6d2528f191568df8a8cf157a
Christian Raoulis <craoulis@googlemail.com>
2022-04-24 12:25:15 +0200

    create biome registry

---
+++ src/main/java/com/borniuus/tensura/registry/BiomeRegistry.java
@@ -0,0 +1,10 @@
+package com.borniuus.tensura.registry;
+
+import net.minecraft.world.level.biome.Biome;
+import net.minecraftforge.registries.DeferredRegister;
+
+class BiomeRegistry {
+    static void register(DeferredRegister<Biome> registry) {
+
+    }
+}
--- src/main/java/com/borniuus/tensura/registry/TensuraRegistry.java
+++ src/main/java/com/borniuus/tensura/registry/TensuraRegistry.java
@@ -4,6 +4,7 @@ import com.borniuus.tensura.Tensura;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.decoration.Motive;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -17,6 +18,7 @@ public class TensuraRegistry {
     private static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, Tensura.MOD_ID);
     private static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, Tensura.MOD_ID);
     private static final DeferredRegister<Motive> MOTIVE = DeferredRegister.create(ForgeRegistries.PAINTING_TYPES, Tensura.MOD_ID);
+    private static final DeferredRegister<Biome> BIOMES = DeferredRegister.create(ForgeRegistries.BIOMES, Tensura.MOD_ID);
 
     /**
      * Registers all registries.
@@ -30,6 +32,7 @@ public class TensuraRegistry {
         BlockEntityTypeRegistry.register(ITEMS, BLOCKS, BLOCK_ENTITY_TYPES); //Registers Block Entities including their Blocks and Items
         SoundEventRegistry.register(SOUND_EVENTS); //Register Sound Events
         MotiveRegistry.register(MOTIVE); //Register Motives for custom paintings
+        BiomeRegistry.register(BIOMES); //Register Biomes
 
         // Add our Registries to Forge
         BLOCKS.register(modEventBus);
@@ -37,5 +40,6 @@ public class TensuraRegistry {
         BLOCK_ENTITY_TYPES.register(modEventBus);
         SOUND_EVENTS.register(modEventBus);
         MOTIVE.register(modEventBus);
+        BIOMES.register(modEventBus);
     }
 }
